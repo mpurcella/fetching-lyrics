741881418f349aed18733d2d90fe3a9aeac9272c
Michael Purcella <mjpurcella@gmail.com>
2022-03-21 12:47:30 -0700

    updated js

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
 let searchBtn = document.querySelector('.search-btn');
 
-searchBtn.addEventListener('click', async (e) => {
+searchBtn.addEventListener('click', (e) => {
 	e.preventDefault();
-	await loadLyrics();
-	document.querySelector('.song-title-input').value = '';
-	document.querySelector('.artist-name-input').value = '';
+	loadLyrics();
 });
 
 async function loadLyrics() {
 	let songTitle = document.querySelector('.song-title-input').value;
 	let artistName = document.querySelector('.artist-name-input').value;
 
-	let response = await fetch(`https://api.lyrics.ovh/v1/${songTitle}/${artistName}`);
-	console.log(response.status);
-	let data = await response.json();
-	console.log(data.lyrics);
-	renderLyrics(data.lyrics);
+	try {
+		let response = await fetch(`https://api.lyrics.ovh/v1/${songTitle}/${artistName}`);
+		let data = await response.json();
+		renderLyrics(data.lyrics);
+		clearInputs();
+	} catch (error) {
+		console.log('Error: ' + error);
+	}
 }
 
 function renderLyrics(lyrics) {
 	document.querySelector('.lyrics').innerText = lyrics;
+}
+
+function clearInputs() {
+	let songTitleInput = document.querySelector('.song-title-input');
+	let artistNameInput = document.querySelector('.artist-name-input');
+
+	songTitleInput.value = '';
+	artistNameInput.value = '';
 }
